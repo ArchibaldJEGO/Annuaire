55fa97752125120b12b647e51b67a51d78ea3729
Archibald Jego <Archibald Jego>
2016-04-20 19:44:53 +0200

    Ajout de la fonction delte et edit

--- liste/urls.py
+++ liste/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^home/$', 'liste.views.home'),
     url(r'^form/$', 'liste.views.formulaire'),
+    url(r'^delete/(?P<id>\d+)$', 'liste.views.delete'),
+    url(r'^edit/(?P<id>\d+)$', 'liste.views.edit'),
 ]
--- liste/views.py
+++ liste/views.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from django.contrib import messages
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.shortcuts import render
 from liste.forms import PersonneForm
 from liste.models import Personne
@@ -12,7 +13,7 @@ def formulaire(request):
         
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/home/')
+            return redirect('/home/')
         
     else:
         form = PersonneForm()
@@ -22,5 +23,30 @@ def formulaire(request):
 def home(request):
     personnes = Personne.objects.all().order_by('nom')
     return render(request, 'liste/liste.html', {'personnes': personnes})
+
+def delete(request, id):
+    personne = get_object_or_404(Personne, id=id)
+    personne.delete()
+    return redirect('/home/')
+
+def edit(request, id):
+    if request.method == 'POST':
+        form = PersonneForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/home/')
+        
+    else:
+        personne = get_object_or_404(Personne, id=id)
+        form = PersonneForm(initial = {
+            'prenom':personne.prenom,
+            'nom':personne.nom,
+            'phone':personne.phone,
+            'email':personne.email
+        })
+        personne.delete()
+
+    return render(request, 'liste/form.html', locals())
 
 # Create your views here.
