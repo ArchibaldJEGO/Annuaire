fe2684154e5c8ee696aed7ecb8243f60fd249b69
Archibald Jego <Archibald Jego>
2016-04-21 12:41:59 +0200

    Ajout de la redirection et nettoyage

--- liste/urls.py
+++ liste/urls.py
@@ -1,11 +1,10 @@
-from . import views
-from django.conf.urls import include
-from django.conf.urls import patterns
 from django.conf.urls import url
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^home/$', 'liste.views.home'),
     url(r'^form/$', 'liste.views.formulaire'),
     url(r'^delete/(?P<id>\d+)$', 'liste.views.delete'),
     url(r'^edit/(?P<id>\d+)$', 'liste.views.edit'),
+    url(r'^$', lambda r: HttpResponseRedirect('home/')),
 ]
--- liste/views.py
+++ liste/views.py
@@ -1,6 +1,3 @@
-from django.contrib import messages
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.shortcuts import render
